ddc3941a7f2d52226ee42e63d6e6452d6023c4d6
Benedikte Busack <benediktebusack1@gmail.com>
2025-10-20 17:20:51 +0200

    lidt start på søgefelt

---
+++ frontend/searchArea.js
@@ -0,0 +1,9 @@
+document.getElementById("searchBtn").addEventListener("click", async () => {
+    const query = document.getElementById("inputArea").Value.trim();
+    const resultEl = document.getElementById("result");
+    if (!query) return resultEl.textContent = "Indtast en sangtitel";
+
+    const res = 
+} )
+
+
