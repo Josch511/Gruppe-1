8618887f1bf65f7595edc4df094b08c62d6e7c2e
XudayfeGuhad <Xudayfe1@outlook.dk>
2025-11-03 10:23:54 +0100

    deleted everything

--- frontend/searchArea.js
+++ frontend/searchArea.js
@@ -1,25 +1,2 @@
-document.getElementById("searchBtn").addEventListener("click", async () => {
-    const query = document.getElementById("inputArea").value.trim();
-    const resultEl = document.getElementById("result");
-    
-    if (!query) {
-        resultEl.textContent = "Indtast en sangtitel";
-        return;
-    }    
-
-    try {
-        const res = await fetch(`http://localhost:3000/search?song=${encodeURIComponent(query)}`);
-        const data = await res.js();
-
-        if (data.found){
-            resultEl.textContent = `Vi fandt sangen "${data.song}" af ${data.artist} fra albummet "${data.album}".`;
-        } else {
-            resultEl.textContent = "Vi kunne dsv ikke finde en sang der matchede din søgning, prøv igen."
-        }
-    } catch (error) {
-        console.error(error);
-        resultEl.textContent = "Der opstod en fejl, prøv igen";
-    }
-});
 
 
