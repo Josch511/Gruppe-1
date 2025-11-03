a71a3c7252bcdd1210313f0f60b076283f00b485
Jonathan Schmidt <josch511@icloud.com>
2025-11-03 10:47:13 +0100

    aegeag

---
+++ backend/searchFunction.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import pg from 'pg';
+import dotenv from 'dotenv';
+import { albums } from './datafile.json';
+
+const server = express();
+
+server.use(express.static("frontend"));
+server.use(express.json());
+server.use(onEachRequest);
+server.get("/datafile.json", onGetTracks);
+
+async function onGetTracks(request, response) {
+    
+}
